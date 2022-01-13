7e57e8af0b06bd53ed36a0eeac3f96cca1a179d0
Kcorb0 <68121651+Kcorb0@users.noreply.github.com>
2022-01-13 00:24:20 +0000

    Day9 part 1 completed

--- day9_smoke_basin.py
+++ day9_smoke_basin.py
@@ -1,2 +1,40 @@
-path = 'inputs/day5_input.txt'
+path = 'inputs/day9_input.txt'
 file = open(path, 'r').read().split('\n')
+
+# Risk level = 1 + Height
+
+risk_sums = []
+
+for idxr, row in enumerate(file):
+
+    for idxc, col in enumerate(row):
+
+        vecinity = []
+
+        #check left
+        if idxc > 0:
+            vecinity.append(file[idxr][idxc-1])
+
+        #check right
+        if idxc < len(row)-1:
+            vecinity.append(file[idxr][idxc+1])
+
+        #check above
+        if idxr > 0:
+            vecinity.append(file[idxr-1][idxc])
+        
+        #check below
+        if idxr < len(file)-1:
+            vecinity.append(file[idxr+1][idxc])
+
+        bottom = True
+        for n in vecinity:
+            if int(n) <= int(col):
+                bottom = False
+        
+        if bottom == True:
+            risk_sums.append(int(col)+1)
+
+print(sum(risk_sums))
+
+
